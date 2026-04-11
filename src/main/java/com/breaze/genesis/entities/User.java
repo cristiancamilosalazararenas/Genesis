dd30a1c096ad13780bc27ecdc91beefacf8b4dc2
kamii02 <camilaauto10@gmail.com>
2026-04-11 18:52:06 -0500

    hotfix: corrección en entidad User antes de finalizar hotfix

--- src/main/java/com/breaze/genesis/entities/User.java
+++ src/main/java/com/breaze/genesis/entities/User.java
@@ -41,4 +41,7 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Subscription> subscriptions;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Rol rol;
 }
